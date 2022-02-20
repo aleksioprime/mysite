711b1ec218f9000ab7497c70d52552804f68dd38
alexprime <alex-x25@yandex.ru>
2022-02-20 16:03:05 +0300

    refactor project

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+from flask import Flask, render_template
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/')
+@app.route('/hello')
+def index():
+    return render_template('base.html', emotion="Привет", name="Yandex")
+
+
+@app.route('/bye')
+def bye():
+    return render_template('base.html', emotion="Пока", name="Yandex")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(host='127.0.0.1', port=8000, debug=True)
