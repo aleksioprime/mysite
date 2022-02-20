d33e289f14b22afc162a16eaaed4b50b204bde03
alexprime <alex-x25@yandex.ru>
2022-02-20 18:56:48 +0300

    Работающая конфигурация web-сервера

--- app.py
+++ app.py
@@ -4,15 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/hello')
 def index():
-    return render_template('base.html', emotion="Привет", name="Yandex")
-
-
-@app.route('/bye')
-def bye():
-    return render_template('base.html', emotion="Пока", name="Yandex")
+    return render_template('base.html', emotion="Привет", name="пользователь")
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=80, debug=True)
